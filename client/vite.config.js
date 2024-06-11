a36cfdbbc2e4bc054d1cdce0f42a91ea0765e8cd
Shivam Singh Yadav <shivamsinghyadavssy888@gmail.com>
2024-06-11 10:40:49 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   server: {
     host: true,
-    port: 80,
+    port: 8080,
     strictPort: true,
     proxy: {
       "/api": {
